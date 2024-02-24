e792357507d6d6fe037c68ee93ceae449a2bde71
nguynkhn <nguynkhan479@gmail.com>
2024-02-24 13:32:06 +0700

    update: telescope find_command

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -2,15 +2,34 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			defaults = {
+				layout_config = {
+					preview_width = 0.5,
+				},
+			},
+			pickers = {
+				find_files = {
+					find_command = {
+						"rg",
+						"--ignore",
+						"--hidden",
+						"--files",
+						"--glob",
+						"!**/.git/*",
+					},
+				},
+			},
+		},
 		keys = {
 			{
 				"<leader>ff",
-				"<cmd>Telescope find_files theme=dropdown<cr>",
+				"<cmd>Telescope find_files<cr>",
 				"Telescope: Find files",
 			},
 			{
 				"<leader>fg",
-				"<cmd>Telescope live_grep theme=dropdown<cr>",
+				"<cmd>Telescope live_grep<cr>",
 				"Telescope: Live grep",
 			},
 		},
