f6f3e465a9d827523a310b50b4c38142908b6f64
nguynkhn <nguynkhan479@gmail.com>
2024-02-27 10:53:21 +0700

    update: startup menu & remove guess-indent

--- .config/nvim/lua/plugins/behaviour.lua
+++ .config/nvim/lua/plugins/behaviour.lua
@@ -3,5 +3,4 @@ return {
 	{ "kylechui/nvim-surround", opts = {} },
 	{ "Wansmer/treesj", opts = { max_join_length = 500 } },
 	{ "numToStr/Comment.nvim", opts = {} },
-	{ "nmac427/guess-indent.nvim", opts = {} },
 }
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -34,4 +34,12 @@ return {
 			},
 		},
 	},
+	{
+		"goolord/alpha-nvim",
+		config = function ()
+			require("alpha").setup(
+				require("alpha.themes.startify").config
+			)
+		end,
+	},
 }
