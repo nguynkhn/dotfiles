5640158295ace066d201bbb29b86eb8e74a967ce
nguynkhn <nguynkhan479@gmail.com>
2024-02-27 11:02:36 +0700

    add: more keybinds for diagnostic

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -8,3 +8,8 @@ keymap.set("n", "sv", vim.cmd.vsplit)
 keymap.set("n", "te", vim.cmd.tabe)
 keymap.set("n", "<Tab>", vim.cmd.tabn)
 keymap.set("n", "<S-Tab>", vim.cmd.tabp)
+
+keymap.set("n", "<space>e", vim.diagnostic.open_float)
+keymap.set("n", "[d", vim.diagnostic.goto_prev)
+keymap.set("n", "]d", vim.diagnostic.goto_next)
+keymap.set("n", "<space>q", vim.diagnostic.setloclist)
