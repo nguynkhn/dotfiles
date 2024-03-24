6d28572c325baee605055df63045e69e159fc69e
nguynkhn <nguynkhan479@gmail.com>
2024-03-24 16:50:54 +0700

    this is nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Home Manager configuration of nguynkhn";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # flake-utils.url = "github:numtide/flake-utils";
+
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, flake-utils, neovim-nightly-overlay, ... }: 
+    let
+      system = "x86_64-linux";
+      overlays = [ neovim-nightly-overlay.overlay ];
+      pkgs = import nixpkgs { inherit system overlays; };
+    in {
+      homeConfigurations."nguynkhn" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+  };
+}
